0a9c79ca7a4bc52a018a44320f80729a0dd57146
sanhaJung <sanha9723@lguplus.co.kr>
2024-07-30 11:52:43 +0900

    spring boot initial setting

---
+++ ToyDesignPattern/src/main/kotlin/org/goodcoder/toydesignpattern/ToyDesignPatternApplication.kt
@@ -0,0 +1,11 @@
+package org.goodcoder.toydesignpattern
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToyDesignPatternApplication
+
+fun main(args: Array<String>) {
+    runApplication<ToyDesignPatternApplication>(*args)
+}
---
+++ ToyDesignPattern/src/test/kotlin/org/goodcoder/toydesignpattern/ToyDesignPatternApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.goodcoder.toydesignpattern
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToyDesignPatternApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
